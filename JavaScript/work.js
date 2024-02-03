af45badd2cb5a86e4f81b4633f6b01caa7772c12
jiku3 <158761172+dzkr1@users.noreply.github.com>
2024-02-04 04:16:20 +0900

    Add files via upload

---
+++ JavaScript/work.js
@@ -0,0 +1,28 @@
+document.getElementById("imageSelectionRow").onclick = function showDiv(n){
+    document.getElementsByClassName("hero").innerhtml = document.getElementsByClassName("imageSelectionRow");
+
+}
+
+
+// function currentDiv(n){
+//     showDivs(slideIndex = n);
+// }
+
+// function showDivs (n){
+//     var i;
+//     var x = document.getElementsByClassName("myslide");
+//     var dots = document.getElementsByClassName("demo");
+//     if(n > x.length) {
+//         slideIndex = 1;
+//     } else if (n < 1) {
+//         slideIndex.length = x.length;
+//     };
+//     for(i = 0; i<x.length; i++){
+//         x[i].style.display = "none";
+//     };
+
+//     // for(i= 0; i< dots.length; i++){
+//     //     dots[i].className = dots[i].className.replace()
+//     // }
+// }
+
